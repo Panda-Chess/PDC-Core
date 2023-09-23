abb51e553703a816373ad0c11168bee96aae1c7b
CRBogdan <bogdanrazvan00@yahoo.com>
2023-09-23 13:41:47 +0300

    ci: test

--- release.config.js
+++ release.config.js
@@ -3,17 +3,8 @@ const config = {
     "plugins": [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
-      ["@semantic-release/git", {
-        "assets": ["dist/*.js", "dist/*.js.map"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
-      [
-        "@semantic-release/github",
-        {
-          "gpr": true,
-          "registryUrl": "https://npm.pkg.github.com"
-        }
-      ]
+      "@semantic-release/github",
+      "@semantic-release/changelog",
     ]
   }
 
