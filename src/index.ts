6724ad9215a0416846553af25a76ecd893467d16
Bogdan <bogdanrazvan00@yahoo.com>
2024-02-26 11:20:16 +0200

    fix: 🐛 export makeMove function

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {generatePieceSet} from "./piece/piece-set-generator.service";
 export {Piece, PieceType, Position} from "./piece/piece-manager.service";
 
 export {Move, getMoves} from "./move/move-generators/move.generators";
+export {makeMove} from "./move/move-maker/move.maker";
