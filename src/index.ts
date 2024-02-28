e3c08e179a06b0c1323d21132348442355ed7561
Bogdan <bogdanrazvan00@yahoo.com>
2024-02-28 15:12:11 +0200

    fix: 🐛 exported GameStates enum

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export {Piece, PieceType, Position} from "./piece/piece-manager.service";
 export {Move, getMoves} from "./move/move-generators/move.generators";
 export {makeMove} from "./move/move-maker/move.maker";
 
-export {checkGameState} from "./game/game-checker";
+export {checkGameState, GameStates} from "./game/game-checker";
