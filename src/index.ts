d80e262bcbd2c865768a7ee1c68af1eed3b778a7
Bogdan <bogdanrazvan00@yahoo.com>
2024-02-28 14:58:20 +0200

    fix: 🐛 exported checkGameState function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export {Piece, PieceType, Position} from "./piece/piece-manager.service";
 
 export {Move, getMoves} from "./move/move-generators/move.generators";
 export {makeMove} from "./move/move-maker/move.maker";
+
+export {checkGameState} from "./game/game-checker";
