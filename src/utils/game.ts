a6b1207854c2eef07c1be28a5426d0d237fa3902
Bogdan <bogdanrazvan00@yahoo.com>
2024-04-12 15:20:06 +0300

    fix: 🐛 added _id to database objects

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -2,6 +2,7 @@ import { Piece } from "../piece/piece-manager.service";
 import { UserInGame } from "./userInGame";
 
 export type Game = {
+    _id: string;
     users: UserInGame[];
     gamePieces: Piece[]; 
     gameType: "casual" | "competitive";
