cb8bd8e4294556ccc1f1d6b1ba4306dd7ed692dd
Bogdan <bogdanrazvan00@yahoo.com>
2024-04-30 13:16:31 +0300

    fix: 🐛 nullable props in Player type

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -1,9 +1,9 @@
 export type Player = {
     name: string;
-    wins: number;
-    losses: number;
-    draws: number;
-    score: number;
+    wins?: number;
+    losses?: number;
+    draws?: number;
+    score?: number;
 
     color: "white" | "black";
 };
