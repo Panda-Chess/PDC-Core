dc697b13043000b6f9a0b0eeadbab877559b1f0e
Bogdan <bogdanrazvan00@yahoo.com>
2024-05-30 23:59:47 +0300

    fix: 🐛 changed casualGame into createGame

--- src/communication/server-events.ts
+++ src/communication/server-events.ts
@@ -2,6 +2,7 @@ import { Socket } from "socket.io";
 import { GameRequestDto } from "./GameRequest.dto";
 import { CasualGameDTO } from "./CasualGame.dto";
 import { Move } from "../move/move-generators/move.generators";
+import { GameTypes } from "../utils";
 
 export enum ServerEvents {
     Connection = "connection",
@@ -16,7 +17,7 @@ export type ServerConnectionEvent = (socket: Socket) => void;
 export type ServerDisconnectEvent = () => void;
 
 export type ServerGameRequestEvent = (gameRequestDTO: GameRequestDto) => void;
-export type ServerCasualGameEvent = (casualGameDTO: CasualGameDTO) => void;
+export type ServerGameCreateEvent = (casualGameDTO: CasualGameDTO, gameType: GameTypes) => void;
 export type ServerMoveEvent = (move: Move) => void;
 
 export type ServerGameContinueEvent = (gameRequestDTO: GameRequestDto) => void;
